bdee6392dbdec2305c54e36d302038365c463c97
RaulLoyola <raul.loy@gmail.com>
2022-11-05 13:00:53 -0600

    Connect to MongoDB and Create User Schema

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,34 @@
 import express from 'express';
 import data from './data.js';
 import cors from 'cors';
+import mongoose from 'mongoose';
+import config from './config.js';
+import userRouter from './routers/userRouter.js';
+
+mongoose
+  .connect(config.MONGODB_URL)
+  .then(() => {
+    console.log('Connected to mongodb');
+  })
+  .catch((error) => {
+    console.log(error.reason);
+  });
 
 const app = express();
 const port = 5000;
 
 app.use(cors());
+
+app.use('/api/users', userRouter);
+
 app.get('/api/products', (req, res) => {
   res.send(data.products);
 });
+
 app.get('/api/banner-products', (req, res) => {
   res.send(data.banner);
 });
+
 app.get('/api/products/:id', (req, res) => {
   const product = data.products.find((product) => product._id == req.params.id);
   if (product) {
